b996b6d17a4f53fc6eac841d5c4991a73dbb7eb7
Fernando Gonzalez <fgonzalez@promaticmedia.cl>
2026-03-01 21:30:40 -0300

    se agregan y eliminan objetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,38 @@ const cuentaTareas = document.getElementById("cuenta-tareas");
 /* Inicializando Arreglo */
 const tareas = []
 
-btnAgregar.addEventListener("click", () => {
-    /* Agregar tareas al arreglo */
-    const nuevaTarea = input.value 
-    tareas.push({id: String(Date.now()).slice(-4), tarea: nuevaTarea})
-    input.value = ""
-
-    /* Actualizamos la info del HTML */
+/* Renderizar HTML */
+function renderTarea() {
+    
     html = ""
 
     for (let tarea of tareas) {
         html += `<tr>
                     <td>${tarea.id}</td>
                     <td>${tarea.tarea}</td>
-                    <td><button>Eliminar</button></td>
+                    <td><button onClick="borrar(${tarea.id})" class="btn-eliminar">Eliminar</button></td>
                     <td></td>
                  </tr>
                 `
     }
     listaDeTareas.innerHTML = html;
+}
 
-
+/* Evento al presionar el botón Agregar */
+btnAgregar.addEventListener("click", () => {
+    
+    /* Agregar tareas al arreglo */
+    const nuevaTarea = input.value 
+    tareas.push({id: String(Date.now()).slice(-4), tarea: nuevaTarea})
+    input.value = ""
+    
+    /* Actualizamos la info del HTML */
+    renderTarea()
 });
+
+/* Funcion para eliminar tareas */
+function borrar(id) {
+    const index = tareas.findIndex((ele) => ele.id === id)
+    tareas.splice(index, 1)
+    renderTarea();
+}
